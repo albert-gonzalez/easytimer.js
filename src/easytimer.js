e8239b3cfedf54336fc43563371f863ea26005f4
Albert Gonzalez <albert.gonzalez@atrapalo.com>
2016-03-03 21:09:33 +0100

    workaround for CustomEvent polyfill

--- src/easytimer.js
+++ src/easytimer.js
@@ -16,15 +16,15 @@ var Timer = (
         /*
          * Polyfill por IE9, IE10 and IE11
          */
+        var CustomEvent = window.CustomEvent;
 
-        function CustomEvent ( event, params ) {
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-            var evt = document.createEvent( 'CustomEvent' );
-            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-            return evt;
-        }
-        
-        if (typeof window.CustomEvent !== "function" ) {
+        if (typeof CustomEvent !== "function" ) {
+            CustomEvent = function ( event, params ) {
+                params = params || { bubbles: false, cancelable: false, detail: undefined };
+                var evt = document.createEvent( 'CustomEvent' );
+                evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+                return evt;
+            };
 
             CustomEvent.prototype = window.Event.prototype;
 
